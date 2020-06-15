df62e1dd5a11ab2e496e7dbbf59b3950444b9601
Varunesh <rai.varunesh@gmail.com>
2020-06-15 20:13:47 +1000

    uat and prd deployments

--- my_artifact_bucket_stack/my_artifact_bucket_stack_stack.py
+++ my_artifact_bucket_stack/my_artifact_bucket_stack_stack.py
@@ -1,9 +1,24 @@
-from aws_cdk import core
+from aws_cdk import(
+    aws_s3 as _s3,
+     core)
 
 
 class MyArtifactBucketStackStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, id: str,is_prod=False, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+
+        if is_prod:
+            artifactBucket = _s3.Bucket(self,
+            "myProdArtifactBucketid",
+            versioned=True,
+            encryption=_s3.BucketEncryption.S3_MANAGED,
+            removal_policy=core.RemovalPolicy.RETAIN)
+
+        else:
+            artifactBucket = _s3.Bucket(self,
+            "muDevArtifactBucketId",
+            removal_policy=core.RemovalPolicy.DESTROY)
+
